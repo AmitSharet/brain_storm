8646baa8a884a6cfa5be7174567e798c817f83be
AmitSharet <amit.sharet@gmail.com>
2020-05-23 20:32:02 +0300

    backup

---
+++ brain_storm/saver/general_saver.py
@@ -0,0 +1,20 @@
+class GeneralSaver:
+    """General Saver is a layer above all savers which gives ageneral api for any db we want to add
+    to our thoughts in the future"""
+
+    def __init__( self, db_url):
+
+        pass
+
+    def __repr__(self):
+        pass
+
+
+    def save(self, data, field):
+        pass
+
+    def get_users(self):
+        pass
+
+    def get_snapshots(self):
+        pass
--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,7 @@ function main {
     find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
     .env/bin/pip install -U pip
     .env/bin/pip install -r requirements.txt
-    docker build -t brain_storm-base:latest .
+    #docker build -t brain_storm-base:latest .
 }
 
 
